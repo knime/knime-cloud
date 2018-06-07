e699a3fb6b098eb1e601f1b90132030b3449c3a9
Jason Tyler <jason.tyler@knime.com>
2018-06-07 14:24:36 -0500

    Initial checkin. Node skeleton

---
+++ org.knime.cloud.google/src/org/knime/cloud/google/drive/node/connector/GoogleDriveConnectionNodeFactory.java
@@ -0,0 +1,41 @@
+package org.knime.cloud.google.drive.node.connector;
+
+import org.knime.core.node.NodeDialogPane;
+import org.knime.core.node.NodeFactory;
+import org.knime.core.node.NodeView;
+
+public class GoogleDriveConnectionNodeFactory extends NodeFactory<GoogleDriveConnectionNodeModel> {
+
+	@Override
+	public GoogleDriveConnectionNodeModel createNodeModel() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	protected int getNrNodeViews() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public NodeView<GoogleDriveConnectionNodeModel> createNodeView(int viewIndex,
+			GoogleDriveConnectionNodeModel nodeModel) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	protected boolean hasDialog() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	protected NodeDialogPane createNodeDialogPane() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+
+}
---
+++ org.knime.cloud.google/src/org/knime/cloud/google/drive/node/connector/GoogleDriveConnectionNodeModel.java
@@ -0,0 +1,68 @@
+package org.knime.cloud.google.drive.node.connector;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.knime.core.node.CanceledExecutionException;
+import org.knime.core.node.ExecutionMonitor;
+import org.knime.core.node.InvalidSettingsException;
+import org.knime.core.node.NodeModel;
+import org.knime.core.node.NodeSettingsRO;
+import org.knime.core.node.NodeSettingsWO;
+import org.knime.core.node.port.PortType;
+
+/**
+*
+* @author Jason Tyler, KNIME.com
+*/
+public class GoogleDriveConnectionNodeModel extends NodeModel {
+
+	public GoogleDriveConnectionNodeModel(int nrInDataPorts, int nrOutDataPorts) {
+		super(nrInDataPorts, nrOutDataPorts);
+		// TODO Auto-generated constructor stub
+	}
+
+	public GoogleDriveConnectionNodeModel(PortType[] inPortTypes, PortType[] outPortTypes) {
+		super(inPortTypes, outPortTypes);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	protected void loadInternals(File nodeInternDir, ExecutionMonitor exec)
+			throws IOException, CanceledExecutionException {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	protected void saveInternals(File nodeInternDir, ExecutionMonitor exec)
+			throws IOException, CanceledExecutionException {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	protected void saveSettingsTo(NodeSettingsWO settings) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	protected void validateSettings(NodeSettingsRO settings) throws InvalidSettingsException {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	protected void loadValidatedSettingsFrom(NodeSettingsRO settings) throws InvalidSettingsException {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	protected void reset() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
