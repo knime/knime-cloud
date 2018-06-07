9bfb9482f8c705f3cf16336199233ed2de03289d
Jason Tyler <jason.tyler@knime.com>
2018-06-07 15:24:35 -0500

    Get bare minimum in place to get node to to where it can be instantiated in the platform.

--- org.knime.cloud.google/src/org/knime/cloud/google/drive/node/connector/GoogleDriveConnectionNodeModel.java
+++ org.knime.cloud.google/src/org/knime/cloud/google/drive/node/connector/GoogleDriveConnectionNodeModel.java
@@ -6,26 +6,36 @@ import java.io.IOException;
 import org.knime.core.node.CanceledExecutionException;
 import org.knime.core.node.ExecutionMonitor;
 import org.knime.core.node.InvalidSettingsException;
+import org.knime.core.node.NodeLogger;
 import org.knime.core.node.NodeModel;
 import org.knime.core.node.NodeSettingsRO;
 import org.knime.core.node.NodeSettingsWO;
 import org.knime.core.node.port.PortType;
+
 
 /**
 *
 * @author Jason Tyler, KNIME.com
 */
 public class GoogleDriveConnectionNodeModel extends NodeModel {
+	
+	// the logger instance
+    private static final NodeLogger logger = NodeLogger
+            .getLogger(GoogleDriveConnectionNodeModel.class);
 
-	public GoogleDriveConnectionNodeModel(int nrInDataPorts, int nrOutDataPorts) {
-		super(nrInDataPorts, nrOutDataPorts);
-		// TODO Auto-generated constructor stub
-	}
-
-	public GoogleDriveConnectionNodeModel(PortType[] inPortTypes, PortType[] outPortTypes) {
-		super(inPortTypes, outPortTypes);
-		// TODO Auto-generated constructor stub
-	}
+    
+    public GoogleDriveConnectionNodeModel() {
+    	super(0, 1);
+    }
+//	public GoogleDriveConnectionNodeModel(int nrInDataPorts, int nrOutDataPorts) {
+//		super(nrInDataPorts, nrOutDataPorts);
+//		// TODO Auto-generated constructor stub
+//	}
+//
+//	public GoogleDriveConnectionNodeModel(PortType[] inPortTypes, PortType[] outPortTypes) {
+//		super(inPortTypes, outPortTypes);
+//		// TODO Auto-generated constructor stub
+//	}
 
 	@Override
 	protected void loadInternals(File nodeInternDir, ExecutionMonitor exec)
