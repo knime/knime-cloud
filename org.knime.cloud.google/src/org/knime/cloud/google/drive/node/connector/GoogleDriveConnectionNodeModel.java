18d817570da551192e9ef9b25489b8e57226c4ab
Jason Tyler <jason.tyler@knime.com>
2018-06-08 16:46:00 -0500

    Incremental checkin. Set correct input and output ports on node. Note, input node is still branded as Google Sheets. Will likely need to alter this to be more generic.

--- org.knime.cloud.google/src/org/knime/cloud/google/drive/node/connector/GoogleDriveConnectionNodeModel.java
+++ org.knime.cloud.google/src/org/knime/cloud/google/drive/node/connector/GoogleDriveConnectionNodeModel.java
@@ -2,15 +2,27 @@ package org.knime.cloud.google.drive.node.connector;
 
 import java.io.File;
 import java.io.IOException;
+import java.net.URI;
 
+import org.knime.base.filehandling.remote.files.ConnectionMonitor;
+import org.knime.base.filehandling.remote.files.RemoteFile;
+import org.knime.base.filehandling.remote.files.RemoteFileFactory;
+import org.knime.cloud.core.util.port.CloudConnectionInformation;
+import org.knime.cloud.core.util.port.CloudConnectionInformationPortObjectSpec;
+import org.knime.cloud.google.util.GoogleDriveConnectionInformationPortObject;
 import org.knime.core.node.CanceledExecutionException;
+import org.knime.core.node.ExecutionContext;
 import org.knime.core.node.ExecutionMonitor;
 import org.knime.core.node.InvalidSettingsException;
 import org.knime.core.node.NodeLogger;
 import org.knime.core.node.NodeModel;
 import org.knime.core.node.NodeSettingsRO;
 import org.knime.core.node.NodeSettingsWO;
+import org.knime.core.node.port.PortObject;
+import org.knime.core.node.port.PortObjectSpec;
 import org.knime.core.node.port.PortType;
+import org.knime.google.api.sheets.data.GoogleSheetsConnection;
+import org.knime.google.api.sheets.data.GoogleSheetsConnectionPortObject;
 
 
 /**
@@ -25,8 +37,11 @@ public class GoogleDriveConnectionNodeModel extends NodeModel {
 
     
     public GoogleDriveConnectionNodeModel() {
-    	super(0, 1);
+    	//super(0, 1);
+    	super(new PortType[]{GoogleSheetsConnectionPortObject.TYPE},
+    			new PortType[] { GoogleDriveConnectionInformationPortObject.TYPE });
     }
+    
 //	public GoogleDriveConnectionNodeModel(int nrInDataPorts, int nrOutDataPorts) {
 //		super(nrInDataPorts, nrOutDataPorts);
 //		// TODO Auto-generated constructor stub
@@ -34,9 +49,29 @@ public class GoogleDriveConnectionNodeModel extends NodeModel {
 //
 //	public GoogleDriveConnectionNodeModel(PortType[] inPortTypes, PortType[] outPortTypes) {
 //		super(inPortTypes, outPortTypes);
-//		// TODO Auto-generated constructor stub
 //	}
 
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    protected PortObject[] execute(final PortObject[] inObjects, final ExecutionContext exec) throws Exception {
+        GoogleSheetsConnection connection =
+            ((GoogleSheetsConnectionPortObject)inObjects[0]).getGoogleSheetsConnection();
+
+        exec.setMessage("Requesting Google Sheet");
+        exec.setMessage(connection.toString());
+        return null;
+    }
+    
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    protected PortObjectSpec[] configure(final PortObjectSpec[] inSpecs) throws InvalidSettingsException {
+        return new PortObjectSpec[]{null};
+    }
+    
 	@Override
 	protected void loadInternals(File nodeInternDir, ExecutionMonitor exec)
 			throws IOException, CanceledExecutionException {
@@ -73,6 +108,36 @@ public class GoogleDriveConnectionNodeModel extends NodeModel {
 	protected void reset() {
 		// TODO Auto-generated method stub
 
+	}
+	
+	/**
+	 * Create the spec, throw exception if no config available.
+	 *
+	 * @return ConnectionInformationPortObjectSpec
+	 * @throws InvalidSettingsException
+	 *             ...
+	 */
+	private CloudConnectionInformationPortObjectSpec createSpec() throws InvalidSettingsException {
+		//m_model.validateValues();
+//		final CloudConnectionInformation connectionInformation = m_model
+//				.createConnectionInformation(getCredentialsProvider(), S3RemoteFileHandler.PROTOCOL);
+//		URI resolve = connectionInformation.toURI().resolve("/");
+//        try {
+//            RemoteFile<S3Connection> s3RemoteFile = RemoteFileFactory.createRemoteFile(resolve, connectionInformation,
+//                new ConnectionMonitor<S3Connection>());
+//            S3Connection connection = s3RemoteFile.getConnection();
+//            if (connection.restrictedPermissions()) {
+//                setWarningMessage("The credentials provided have restricted permissions. "
+//                    + "File browsing in the Remote File nodes might not work as expected.\n"
+//                    + "All buckets will be assumed existing, as they cannot be listed.");
+//            }
+//		} catch (InvalidSettingsException ex) {
+//		    throw ex;
+//		} catch (Exception ex) {
+//	        throw new InvalidSettingsException(ex.getMessage());
+//		}
+//		return new CloudConnectionInformationPortObjectSpec(connectionInformation);
+		return null;
 	}
 
 }
---
+++ org.knime.cloud.google/src/org/knime/cloud/google/util/GoogleDriveConnectionInformationPortObject.java
@@ -0,0 +1,44 @@
+package org.knime.cloud.google.util;
+
+import org.knime.base.filehandling.remote.connectioninformation.port.ConnectionInformationPortObject;
+import org.knime.cloud.core.util.port.CloudConnectionInformationPortObjectSpec;
+import org.knime.core.node.port.PortType;
+
+/**
+*
+* @author Jason Tyler, KNIME.com
+*/
+public class GoogleDriveConnectionInformationPortObject extends ConnectionInformationPortObject {
+	
+	/**
+     * @noreference This class is not intended to be referenced by clients.
+     */
+    public static final class Serializer extends AbstractSimplePortObjectSerializer<GoogleDriveConnectionInformationPortObject> {}
+
+    /**
+	 * No-argument constructor for framework
+	 */
+	public GoogleDriveConnectionInformationPortObject() {
+	}
+
+    /**
+     * Type of this port.
+     */
+    public static final PortType TYPE = ConnectionInformationPortObject.TYPE;
+
+    /**
+     * Type of this optional port.
+     */
+    public static final PortType TYPE_OPTIONAL = ConnectionInformationPortObject.TYPE_OPTIONAL;
+
+    /**
+     * Creates a port object with the given connection information.
+     *
+     * @param connectionInformationPOS The spec wrapping the connection
+     *            information.
+     */
+    public GoogleDriveConnectionInformationPortObject(final CloudConnectionInformationPortObjectSpec connectionInformationPOS) {
+        super(connectionInformationPOS);
+    }
+
+}
