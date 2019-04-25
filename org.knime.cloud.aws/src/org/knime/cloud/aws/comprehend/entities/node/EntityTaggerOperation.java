d93846d5948073cf8844473df27f1966fa840158
Jim Falgout <jim.falgout@knime.com>
2019-04-25 17:06:12 -0500

    Converting entity node to a Document tagger.

--- org.knime.cloud.aws/src/org/knime/cloud/aws/comprehend/entities/node/EntityTaggerOperation.java
+++ org.knime.cloud.aws/src/org/knime/cloud/aws/comprehend/entities/node/EntityTaggerOperation.java
@@ -44,13 +44,9 @@
  * ---------------------------------------------------------------------
  *
  * History
- *   Mar 16, 2019 (jfalgout): created
+ *   Apr 24, 2019 (jfalgout): created
  */
 package org.knime.cloud.aws.comprehend.entities.node;
-
-import java.util.Arrays;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
 import org.knime.base.filehandling.remote.connectioninformation.port.ConnectionInformation;
 import org.knime.cloud.aws.comprehend.BaseComprehendOperation;
@@ -60,9 +56,6 @@ import org.knime.core.data.DataRow;
 import org.knime.core.data.DataTableSpec;
 import org.knime.core.data.RowKey;
 import org.knime.core.data.def.DefaultRow;
-import org.knime.core.data.def.DoubleCell;
-import org.knime.core.data.def.IntCell;
-import org.knime.core.data.def.StringCell;
 import org.knime.core.node.CanceledExecutionException;
 import org.knime.core.node.ExecutionContext;
 import org.knime.core.node.streamable.RowInput;
@@ -70,43 +63,52 @@ import org.knime.core.node.streamable.RowOutput;
 import org.knime.ext.textprocessing.data.Document;
 import org.knime.ext.textprocessing.data.DocumentCell;
 import org.knime.ext.textprocessing.data.DocumentValue;
-import org.knime.ext.textprocessing.data.Tag;
-import org.knime.ext.textprocessing.data.Term;
-import org.knime.ext.textprocessing.data.TermCell2;
-import org.knime.ext.textprocessing.data.Word;
+import org.knime.ext.textprocessing.nodes.tagging.DocumentTagger;
 
 import com.amazonaws.services.comprehend.AmazonComprehend;
-import com.amazonaws.services.comprehend.model.DetectEntitiesRequest;
-import com.amazonaws.services.comprehend.model.DetectEntitiesResult;
-import com.amazonaws.services.comprehend.model.Entity;
 
 /**
+ * The operation that is executed for each partition of the input data. This operation accesses the input
+ * <code>Document</code> column that is to be analyzed and applies the entity tagger to the document.
+ * The AWS Comprehend service is used to detect entities within sentences of the document. The tagger creates
+ * a new document that contains the tagged entities.
  *
- * Support streaming the entity discovery computation.
+ * @author jfalgout
  */
-/* protected */ class EntityOperation extends BaseComprehendOperation {
+class EntityTaggerOperation extends BaseComprehendOperation {
 
     // Language of the source text to be analyzed
     private final String m_sourceLanguage;
+    private final String m_tokenizerName;
 
-    EntityOperation(final ConnectionInformation cxnInfo, final String textColumnName, final String sourceLanguage, final DataTableSpec outputSpec) {
+    EntityTaggerOperation(final ConnectionInformation cxnInfo, final String textColumnName, final String sourceLanguage, final String tokenizerName, final DataTableSpec outputSpec) {
         super(cxnInfo, textColumnName, outputSpec);
         this.m_sourceLanguage = sourceLanguage;
+        this.m_tokenizerName = tokenizerName;
     }
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public void compute(final RowInput in, final RowOutput out, final ExecutionContext exec, final long rowCount) throws CanceledExecutionException, InterruptedException {
 
         // Create a connection to the Comprehend service in the provided region
         AmazonComprehend comprehendClient = ComprehendUtils.getClient(m_cxnInfo);
 
+        // Create the tagger that uses the detect entities capability of the Comprehend service.
+        DocumentTagger tagger =
+                new EntityTagger(
+                    comprehendClient,
+                    ComprehendUtils.LANG_MAP.getOrDefault(m_sourceLanguage, "en"),
+                    m_tokenizerName);
+
         // Access the input data table
         int textColumnIdx = in.getDataTableSpec().findColumnIndex(m_textColumnName);
         long inputRowIndex = 0;
         long rowCounter = 0;
 
-        // For each input row, grab the text column, make the call to Comprehend
-        // and push each of the syntax elements to the output.
+        // Tag each input document
         DataRow inputRow = null;
         while ((inputRow = in.poll()) != null) {
 
@@ -119,59 +121,30 @@ import com.amazonaws.services.comprehend.model.Entity;
 
             // Grab the text to evaluate
             Document inputDoc = ((DocumentValue) inputRow.getCell(textColumnIdx)).getDocument();
-            String textValue = inputDoc.getDocumentBodyText();
 
-            DetectEntitiesRequest request =
-                    new DetectEntitiesRequest()
-                        .withText(textValue)
-                        .withLanguageCode(ComprehendUtils.LANG_MAP.getOrDefault(m_sourceLanguage, "en"));
+            // Apply the entity tagger to the input document.
+            Document outputDoc = tagger.tag(inputDoc);
 
-            DetectEntitiesResult result =
-                comprehendClient
-                    .detectEntities(request);
+            RowKey key = new RowKey("Row " + inputRowIndex);
 
-            // Process the results
-            long outputRowIndex = 0;
-            for (Entity entity : result.getEntities()) {
-
-
-                // Make row key unique with the input row number and the sequence number of each token
-                RowKey key = new RowKey("Row " + inputRowIndex + "_" + outputRowIndex++);
-
-                // Create cells containing the output data.
-                // Copy the input data to the output
-                int numInputColumns = inputRow.getNumCells();
-                DataCell[] cells = new DataCell[numInputColumns + 7];
-                for (int i = 0; i < numInputColumns; i++) {
-                    cells[i] = inputRow.getCell(i);
-                }
-
-                // Copy the discovered entity info to the output.
-                cells[numInputColumns] = new DocumentCell(inputDoc);
-                cells[numInputColumns + 1] = new StringCell(entity.getText());
-                cells[numInputColumns + 2] = new StringCell(entity.getType());
-                cells[numInputColumns + 3] = new DoubleCell(entity.getScore());
-                cells[numInputColumns + 4] = new IntCell(entity.getBeginOffset());
-                cells[numInputColumns + 5] = new IntCell(entity.getEndOffset());
-                cells[numInputColumns + 6] = new TermCell2(createTerm(entity.getText(), entity.getType()));
-
-                // Create a new data row and push it to the output container.
-                DataRow row = new DefaultRow(key, cells);
-                out.push(row);
+            // Create cells containing the output data.
+            // Copy the input data to the output
+            int numInputColumns = inputRow.getNumCells();
+            DataCell[] cells = new DataCell[numInputColumns + 1];
+            for (int i = 0; i < numInputColumns; i++) {
+                cells[i] = inputRow.getCell(i);
             }
+
+            // Copy the output document tagged with entities to the output
+            cells[numInputColumns] = new DocumentCell(outputDoc);
+
+            // Create a new data row and push it to the output container.
+            DataRow row = new DefaultRow(key, cells);
+            out.push(row);
 
             ++inputRowIndex;
         }
 
-        return;
-    }
-
-    private static Term createTerm(final String entity, final String tagValue) {
-
-        return new Term(
-            Stream.of(entity.split(" ")).map(word -> new Word(word, " ")).collect(Collectors.toList()),
-            Arrays.asList(new Tag[] { new Tag(tagValue, "AWS")}),
-            false);
     }
 
 }
